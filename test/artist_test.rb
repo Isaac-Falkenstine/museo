a8b668ee6b5c245f32665dd3b840f97f7f2f7bf6
Isaac Falkenstine <isaac@falkenstine.com>
2018-09-18 09:17:16 -0600

    Added all iteration 1 functionailty for artist and testing for it

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -1,12 +1,42 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/customer'
-require './lib/dog'
-require './lib/day_care'
+require './lib/artist'
 require 'pry'
 
-class DayCareTest < MiniTest::Test
-  def test_it_exists
-    day_care = DayCare.new("The Dog Spot")
-    assert_instance_of DayCare, day_care
+class ArtistTest < MiniTest::Test
+  def setup
+    attributes = {
+            id: "2",
+            name: "Ansel Adams",
+            born: "1902",
+            died: "1984",
+            country: "United States"
+          }
+    @artist = Artist.new(attributes)
   end
+
+  def test_it_exists
+    assert_instance_of Artist, @artist
+  end
+
+  def test_it_can_return_its_id
+    assert_equal "2", @artist.id
+  end
+
+  def test_it_can_return_its_name
+    expected = "Ansel Adams"
+    assert_equal expected, @artist.name
+  end
+
+  def test_it_can_return_the_artist_birth_year
+    assert_equal "1902", @artist.born
+  end
+
+  def test_it_can_return_the_artist_death_year
+    assert_equal "1984", @artist.died
+  end
+
+  def test_it_can_return_the_artist_country
+    assert_equal "United States", @artist.country
+  end
+end
